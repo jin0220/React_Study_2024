b5bba17ccb90fc6924f791bbc468d92aa5796159
jin0220 <jin__0220@naver.com>
2025-03-09 21:14:03 +0900

    useEffect 활용

--- section02/src/App.jsx
+++ section02/src/App.jsx
@@ -1,10 +1,36 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState } from 'react'
+import Even from './components/Even'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트 : 변화, 리렌더링
+  useEffect(() => {
+    if(!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  })
+
+  // 3. 언마운트 : 죽음
+
+
+  // useEffect(() => {
+  //   console.log(`count : ${count} / input: ${input}`);
+  // }, [count, input]) //두 번째 인자로 들어간 배열의 값이 변경되면 콜백함수 실행. 
+              //해당 배열을 의존성 배열이라고 하며, dependency array, 줄여서 deps라고 함.
 
   const onClickButton = (value) => {
     setCount(count + value);
@@ -14,7 +40,16 @@ function App() {
     <div className='App'>
       <h1>Simple Counter</h1>
       <section>
+        <input 
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
         <Viewer count={count}/>
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
