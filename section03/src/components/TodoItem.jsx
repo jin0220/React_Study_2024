668949e9c1b20ab42519ae854d7bfdde1abce4eb
jin0220 <jin__0220@naver.com>
2025-03-15 15:55:53 +0900

    TodoList

---
+++ section03/src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import './TodoItem.css';
+
+const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
+    const onChangCheckbox = () => {
+        onUpdate(id);
+    }
+
+    const onClickDeletButton = () => {
+        onDelete(id);
+    }
+    
+    return (
+        <div className="TodoItem">
+            <input onChange={onChangCheckbox} readOnly checked={isDone} type="checkbox" />
+            <div className="content">{content}</div>
+            <div className="date">{new Date(date).toLocaleDateString()}</div>
+            <button onClick={onClickDeletButton}>삭제</button>
+        </div>
+    )
+}
+
+export default TodoItem;
